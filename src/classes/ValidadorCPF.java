5c677af26109f7fb4e08087e11419b6076de3f22
Matheus Felipe Alves Durães <mateusfelipe10@live.com>
2023-09-15 20:18:32 -0400

    style: Adequamento de nomes de variaveis

--- src/classes/ValidadorCPF.java
+++ src/classes/ValidadorCPF.java
@@ -10,45 +10,45 @@ public class ValidadorCPF {
 	        CPF.equals("88888888888") || CPF.equals("99999999999") ||
 	        CPF.length() != 11) return(false);
 	
-	    char dig10, dig11;
-	    int sm, i, r, num, peso;
+	    char digitoDez, digitoOnze;
+	    int soma, resto, auxiliar, peso;
 	
 	    // "try" - protege o codigo para eventuais erros de conversao de tipo (int)
 	    try {
 		    // Calculo do 1o. Digito Verificador
-	        sm = 0;
+	        soma = 0;
 	        peso = 10;
-	        for (i=0; i<9; i++) {
+	        for (int i=0; i<9; i++) {
 			    // converte o i-esimo caractere do CPF em um numero:
 			    // por exemplo, transforma o caractere '0' no inteiro 0
 			    // (48 eh a posicao de '0' na tabela ASCII)
-		        num = (int)(CPF.charAt(i) - 48);
-		        sm = sm + (num * peso);
+		        auxiliar = (int)(CPF.charAt(i) - 48);
+		        soma = soma + (auxiliar * peso);
 		        peso = peso - 1;
 	        }
 	
-	        r = 11 - (sm % 11);
-	        if ((r == 10) || (r == 11))
-	            dig10 = '0';
+	        resto = 11 - (soma % 11);
+	        if ((resto == 10) || (resto == 11))
+	            digitoDez = '0';
 	        
-	        else dig10 = (char)(r + 48); // converte no respectivo caractere numerico
+	        else digitoDez = (char)(resto + 48); // converte no respectivo caractere numerico
 	
 	        // Calculo do 2o. Digito Verificador
-	        sm = 0;
+	        soma = 0;
 	        peso = 11;
-	        for(i=0; i<10; i++) {
-		        num = (int)(CPF.charAt(i) - 48);
-		        sm = sm + (num * peso);
+	        for(int i=0; i<10; i++) {
+		        auxiliar = (int)(CPF.charAt(i) - 48);
+		        soma = soma + (auxiliar * peso);
 		        peso = peso - 1;
 	        }
 	
-	        r = 11 - (sm % 11);
-	        if ((r == 10) || (r == 11))
-	             dig11 = '0';
-	        else dig11 = (char)(r + 48);
+	        resto = 11 - (soma % 11);
+	        if ((resto == 10) || (resto == 11))
+	             digitoOnze = '0';
+	        else digitoOnze = (char)(resto + 48);
 	
 	        // Verifica se os digitos calculados conferem com os digitos informados.
-	        if ((dig10 == CPF.charAt(9)) && (dig11 == CPF.charAt(10)))
+	        if ((digitoDez == CPF.charAt(9)) && (digitoOnze == CPF.charAt(10)))
 	             return(true);
 	        
 	        else return(false);
