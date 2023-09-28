d22d740d1c0550285ed6ee13aadd7cdd3b5542cc
PedroOSilv <81320895+PedroOSilv@users.noreply.github.com>
2023-09-28 15:06:12 -0400

    Feature: Insert Do Objeto Cliente no Banco

--- src/Principal.java
+++ src/Principal.java
@@ -1,9 +1,31 @@
+import classes.Cliente;
+import classes.UnidadeFederativa;
 import conexaoBanco.ConexaoMySQL;
 
 public class Principal {
 	public static void main(String[] args) {
 		System.out.println("Bem vindo, testando conexão!");
 		
+//		public Cliente(String CPF, String nome, String email, String telefone, 
+//				int diaDoNascimento, int mesDoNascimento, int anoDoNascimento)
+		
+//		String sql = "INSERT INTO Cliente VALUES ('08010842109',"
+//				+ "'Pedro Paulo',"
+//				+ "'Gumercindo',"
+//				+ "'Centro',"
+//				+ "'Terenos',"
+//				+ "'pedroapsilva22@gmail.com',"
+//				+ "'13/05/2002',"
+//				+ "1061,\r\n"
+//				+ "99881718,"
+//				+ "02)";
+		
+		Cliente c = new Cliente("08010842109","Pedro Paulo Araujo","pedroapsilva22@gmail.com","99881718",13,05,2002);
+		c.setBairro("Centro");
+		c.setCidade("Terenos");
+		c.setRua("Gumercindo");
+		c.setNumeroResidencia(1061);
+		c.setUF(UnidadeFederativa.MS);
 		
 		ConexaoMySQL connection = new ConexaoMySQL();
 		System.out.println(connection.statusConection());
@@ -12,12 +34,15 @@ public class Principal {
 		System.out.println(connection.statusConection());
 		
 		System.out.println("Inserindo Cliente");
-		connection.InserirCliente();
-		System.out.println("Cliente inserido com sucesso");
-		
-		
+		System.out.println(connection.InserirCliente(c));
 		
 		connection.FecharConexao();
+		
+		
+		
+		
+		
+		
 	
 	}
 }
--- src/conexaoBanco/ConexaoMySQL.java
+++ src/conexaoBanco/ConexaoMySQL.java
@@ -8,25 +8,28 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
+import classes.Cliente;
+
 //Início da classe de conexão//
 
 public class ConexaoMySQL {
 	public static String status = "Não conectou...";
 
-	//Método Construtor da Classe//
-	public ConexaoMySQL() {}
+	// Método Construtor da Classe//
+	public ConexaoMySQL() {
+	}
 
-	//Método de Conexão//
+	// Método de Conexão//
 	public static java.sql.Connection getConexaoMySQL() {
 
 		Connection connection = null; // atributo do tipo Connection
 		try {
-			//Carregando o JDBC Driver padrão
+			// Carregando o JDBC Driver padrão
 
 			String driverName = "com.mysql.cj.jdbc.Driver";
 			Class.forName(driverName);
 
-			//Configurando a nossa conexão com um banco de dados//
+			// Configurando a nossa conexão com um banco de dados//
 			String serverName = "localhost"; // caminho do servidor do BD
 			String mydatabase = "exgrupo"; // nome do seu banco de dados
 			String url = "jdbc:mysql://" + serverName + "/" + mydatabase;
@@ -41,12 +44,12 @@ public class ConexaoMySQL {
 				status = ("STATUS--->Não foi possivel realizar conexão");
 			}
 			return connection;
-			
+
 		} catch (ClassNotFoundException e) { // Driver não encontrado
 			System.out.println("O driver expecificado nao foi encontrado.");
 			return null;
 		} catch (SQLException e) {
-			//Não conseguindo se conectar ao banco
+			// Não conseguindo se conectar ao banco
 
 			System.out.println("Nao foi possivel conectar ao Banco de Dados.");
 			return null;
@@ -74,28 +77,46 @@ public class ConexaoMySQL {
 		FecharConexao();
 		return ConexaoMySQL.getConexaoMySQL();
 	}
-	
-	public static boolean InserirCliente(){
-		
-		String sql = "INSERT INTO Cliente VALUES ('08010842109','Pedro Paulo',\r\n"
-				+ "'Gumercindo','Centro','Terenos','pedroapsilva22@gmail.com','13/05/2002',1061,\r\n"
-				+ "99881718,02)";
+
+	public static boolean InserirCliente(Cliente cliente) {
+
+//		String sql = "INSERT INTO Cliente VALUES ('08010842109',"
+//				+ "'Pedro Paulo',"
+//				+ "'Gumercindo',"
+//				+ "'Centro',"
+//				+ "'Terenos',"
+//				+ "'pedroapsilva22@gmail.com',"
+//				+ "'13/05/2002',"
+//				+ "1061,\r\n"
+//				+ "99881718,"
+//				+ "02)";
+
+		// Modify sql table to column dataNascimento receive date
+
+		String sql = "INSERT INTO Cliente VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 		try {
-			
+
 			PreparedStatement statement = ConexaoMySQL.getConexaoMySQL().prepareStatement(sql);
-//			statement.setString(1, "bill");
-//			statement.setString(2, "secretpass");
-//			statement.setString(3, "Bill Gates");
-//			statement.setString(4, "bill.gates@microsoft.com");
-		
+			statement.setString(1, cliente.getCPF());
+			statement.setString(2, cliente.getNome());
+			statement.setString(3, cliente.getRua());
+			statement.setString(4, cliente.getBairro());
+			statement.setString(5, cliente.getCidade());
+			statement.setString(6, cliente.getEmail());
+			statement.setString(7, cliente.getDataNascimento().getData());
+			statement.setInt(8, cliente.getNumeroResidencia());
+			statement.setString(9, cliente.getTelefone());
+			statement.setInt(10, cliente.getUF().ordinal());
+
 			int rowsInserted = statement.executeUpdate();
 			if (rowsInserted > 0) {
-			    System.out.println("A new user was inserted successfully!");
+				System.out.println("A new user was inserted successfully!");
 			}
 			return true;
 		} catch (SQLException e) {
+			System.out.println(e);
 			return false;
 		}
 	}
-		
-	}
+
+}
