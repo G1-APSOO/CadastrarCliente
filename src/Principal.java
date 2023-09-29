608c93f1a4b95fca01165441eb2e7b49336987a5
PedroOSilv <81320895+PedroOSilv@users.noreply.github.com>
2023-09-29 16:51:05 -0400

    Feature: CRUD Busca Cliente

--- src/Principal.java
+++ src/Principal.java
@@ -5,44 +5,30 @@ import conexaoBanco.ConexaoMySQL;
 public class Principal {
 	public static void main(String[] args) {
 		System.out.println("Bem vindo, testando conexão!");
-		
-//		public Cliente(String CPF, String nome, String email, String telefone, 
-//				int diaDoNascimento, int mesDoNascimento, int anoDoNascimento)
-		
-//		String sql = "INSERT INTO Cliente VALUES ('08010842109',"
-//				+ "'Pedro Paulo',"
-//				+ "'Gumercindo',"
-//				+ "'Centro',"
-//				+ "'Terenos',"
-//				+ "'pedroapsilva22@gmail.com',"
-//				+ "'13/05/2002',"
-//				+ "1061,\r\n"
-//				+ "99881718,"
-//				+ "02)";
-		
-		Cliente c = new Cliente("08010842109","Pedro Paulo Araujo","pedroapsilva22@gmail.com","99881718",13,05,2002);
+
+		Cliente c = new Cliente("08010842109", "Pedro Paulo Araujo", "pedroapsilva22@gmail.com", "99881718", 13, 05,
+				2002);
 		c.setBairro("Centro");
 		c.setCidade("Terenos");
 		c.setRua("Gumercindo");
 		c.setNumeroResidencia(1061);
 		c.setUF(UnidadeFederativa.MS);
-		
+
 		ConexaoMySQL connection = new ConexaoMySQL();
 		System.out.println(connection.statusConection());
 
 		connection.getConexaoMySQL();
 		System.out.println(connection.statusConection());
-		
+
 		System.out.println("Inserindo Cliente");
 		System.out.println(connection.InserirCliente(c));
-		
+
+		System.out.println("bucando cliente:");
+		Cliente c2 = connection.BuscarCliente("08010842109");
+		System.out.println(c2.getNome());
+		System.out.println(c2.getTelefone());
+
 		connection.FecharConexao();
-		
-		
-		
-		
-		
-		
-	
+
 	}
 }
